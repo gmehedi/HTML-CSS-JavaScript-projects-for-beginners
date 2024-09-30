bf166fdada804d47af71ff8261e4bb1330b26e49
gmehedi <mehedi.self@gmail.com>
2024-09-30 18:24:24 +0600

    Solution Option UI competed

--- projects/My Project/index.js
+++ projects/My Project/index.js
@@ -1,11 +1,13 @@
 const toggleButton = document.querySelector('.solutions');
 const solutionHView = document.querySelector('.solutions-menu-holder-view');
+const solutionItemsHView = document.querySelector('.solutions-menu-items-holder-view');
+
 const arrowView = document.querySelector('.down');
 
 toggleButton.addEventListener('click', function() {
     // Check if the solutions menu is currently visible
     solutionHView.classList.toggle('show'); // Hide the menu with animation
-
+    solutionItemsHView.classList.toggle('show')
     arrowView.classList.toggle('downArrowRotate'); // Rotate with animation
    
 });
